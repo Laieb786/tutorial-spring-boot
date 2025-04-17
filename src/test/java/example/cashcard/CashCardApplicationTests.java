c6c612f50249daed9dbf5b3b420ec35b0b877c01
Laieb786 <laieb.ahmed@gmail.com>
2025-04-17 13:23:22 +0200

    Implementierung des POST-API-Endpoints

--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -3,11 +3,13 @@ package example.cashcard;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Null;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import java.net.URI;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -48,6 +50,19 @@ class CashCardApplicationTests {
 	// Dies ist ein Test für den POST-Endpunkt, der eine neue CashCard erstellt
 	@Test
 	void shouldCreateANewCashCard() {
+		CashCard newCashCard = new CashCard(null, 250.0); // ID ist null, da sie automatisch generiert werden soll
+		ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
+		assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
 
+		URI locationOfNewCashCard = createResponse.getHeaders().getLocation();
+		ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewCashCard, String.class);
+		assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
+		Number id = documentContext.read("$.id");
+		Double amount = documentContext.read("$.amount");
+
+		assertThat(id).isNotNull();
+		assertThat(amount).isEqualTo(250.00);
 	}
 }
